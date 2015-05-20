e711127ec37431c6bd4d1336068f4faaa982c556
nstanzani <nstanzani04@gmail.com>
2015-05-20 19:17:10 -0300

    Teste final

---
+++ src/br/usp/icmc/ssc0103/Date.java
@@ -0,0 +1,21 @@
+package br.usp.icmc.ssc0103;
+
+/**
+ * Created by naldost on 19/05/15.
+ */
+public class Date {
+    private final int day;
+    private final int month;
+    private final int year;
+
+    Date(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString(){
+        return day + "/" + month + "/" + year;
+    }
+}
